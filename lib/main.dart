e13b52c67f8da829637743fdffc51096d814959b
cronus6w6 <seer20582609@gmail.com>
2020-12-22 23:15:19 +0800

    feat: detail page

--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,15 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text('及時AQI查詢'),
-          backgroundColor: Colors.cyan[400],
+          title: Text('即時 AQI 查詢'),
+          backgroundColor: Colors.blue[200],
           actions: <Widget>[
             IconButton(
               icon: Icon(
                 Icons.search,
                 color: Colors.white,
               ),
-              onPressed: () {
-                
-              },
+              onPressed: () {},
             )
           ],
         ),
@@ -49,7 +47,7 @@ class AQIList extends StatefulWidget {
 }
 
 class _AQIListState extends State<AQIList> {
-  final Map<String, Color> AQIColors = {
+  final Map<String, Color> aqiColors = {
     "良好": Colors.green,
     "普通": Colors.yellow[700],
     "對敏感族群不健康": Colors.orange[700],
@@ -70,99 +68,162 @@ class _AQIListState extends State<AQIList> {
   Widget build(BuildContext context) {
     return Container(
       child: FutureBuilder<List<Station>>(
-          future: futureStationList,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                padding: EdgeInsets.all(5),
-                itemBuilder: (context, index) {
-                  Color AQIColor = AQIColors[snapshot.data[index].aqi_status];
+        future: futureStationList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              padding: EdgeInsets.all(5),
+              physics: const AlwaysScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                Color aqiColor = aqiColors[snapshot.data[index].aqi_status];
 
-                  return Container(
-                    margin: EdgeInsets.only(top: 10, bottom: 10),
-                    child: ListTile(
-                      subtitle: Text(snapshot.data[index].aqi_status),
-                      title: Text(
-                        snapshot.data[index].siteName,
-                        style: TextStyle(fontSize: 24, color: Colors.blue[400]),
-                      ),
-                      leading: Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                            color: AQIColor,
-                            borderRadius: BorderRadius.all(Radius.circular(50)),
-                            border: Border.all(
-                                width: 1.5,
-                                color: AQIColor,
-                                style: BorderStyle.solid)),
-                        child: Center(
-                            child: Text(snapshot.data[index].aqi.toString(),
-                                style: TextStyle(
-                                    fontSize: 22, color: Colors.white))),
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context, 
-                          MaterialPageRoute(
-                            builder: (
-                              (context) => AQIDetail(snapshot.data[index], AQIColor)
-                            )
-                          )
-                        );
-                        print('$index');
-                      },
+                return Container(
+                  margin: EdgeInsets.only(top: 10, bottom: 10),
+                  child: ListTile(
+                    subtitle: Text(snapshot.data[index].aqi_status),
+                    title: Text(
+                      snapshot.data[index].siteName,
+                      style: TextStyle(fontSize: 24, color: Colors.blue),
                     ),
-                  );
-                },
-                itemCount: snapshot.data.length,
-              );
-            } else {}
+                    leading: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: aqiColor,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50),
+                        ),
+                        border: Border.all(
+                            width: 1.5,
+                            color: aqiColor,
+                            style: BorderStyle.solid),
+                      ),
+                      child: Center(
+                        child: Text(
+                          snapshot.data[index].aqi.toString(),
+                          style: TextStyle(fontSize: 22, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: ((context) =>
+                              AQIDetail(snapshot.data[index], aqiColor)),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+              itemCount: snapshot.data.length,
+            );
+          } else {}
 
-            return Center(
-                child: CircularProgressIndicator(
-              strokeWidth: 10,
-              backgroundColor: Colors.teal[100],
-              valueColor: new AlwaysStoppedAnimation<Color>(Colors.teal),
-            ));
-          }),
+          return Center(
+            child: CircularProgressIndicator(
+              strokeWidth: 5,
+              backgroundColor: Colors.blue[100],
+              valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue[600]),
+            ),
+          );
+        },
+      ),
     );
   }
 }
 
 class AQIDetail extends StatelessWidget {
   final Station station;
-  final Color status_color;
+  final Color statusColor;
 
-  AQIDetail(this.station, this.status_color) : super();
+  AQIDetail(this.station, this.statusColor) : super();
 
   @override
   Widget build(BuildContext context) {
+    const detailBorder = BorderSide(width: 1.0, color: Colors.black12);
+    const detailDecoration =
+        BoxDecoration(border: Border(bottom: detailBorder));
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue[200],
         title: Text(station.siteName),
       ),
-      body: Container(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "空氣品質：",
-              style: TextStyle(fontSize: 20),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 30),
-              child: Text(
-                station.aqi_status,
-                style: TextStyle(
-                  color: status_color,
-                  fontSize: 40,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "空氣品質：",
+                style: TextStyle(fontSize: 20),
+              ),
+              Container(
+                width: double.infinity,
+                decoration: detailDecoration,
+                padding: EdgeInsets.only(left: 30),
+                child: Text(
+                  station.aqi_status,
+                  style: TextStyle(
+                    color: statusColor,
+                    fontSize: 40,
+                  ),
                 ),
               ),
-            ),
-          ],
+              DetailRow("AQI", station.aqi.toString(), ""),
+              DetailRow("PM2.5", station.pm25, "μg/m3"),
+              DetailRow("PM10", station.pm10, "μg/m3"),
+              DetailRow("風速", station.windSpeed, "m/sec"),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class DetailRow extends StatelessWidget {
+  final String title;
+  final String value;
+  final String unit;
+
+  DetailRow(this.title, this.value, this.unit) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    const detailBorder = BorderSide(width: 1.0, color: Colors.black12);
+    const detailDecoration =
+        BoxDecoration(border: Border(bottom: detailBorder));
+    const detailPadding = EdgeInsets.only(top: 10, bottom: 10);
+    return Container(
+      width: double.infinity,
+      padding: detailPadding,
+      decoration: detailDecoration,
+      child: Row(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.only(right: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.blue[900],
+            ),
+            child: Text(
+              this.title,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Text(
+            this.value + " " + this.unit,
+            style: TextStyle(fontSize: 24),
+          )
+        ],
       ),
     );
   }
--- lib/station.dart
+++ lib/station.dart
@@ -2,6 +2,9 @@ class Station {
   String siteName;
   String aqi_status;
   int aqi;
+  String pm25;
+  String pm10;
+  String windSpeed;
 
   Station({this.siteName});
 
@@ -9,5 +12,8 @@ class Station {
     siteName = json['SiteName'];
     aqi = int.tryParse(json['AQI']) ?? -1;
     aqi_status = json['Status'];
+    pm25 = json['PM2.5'] ?? "無";
+    pm10 = json['PM10'] ?? "無";
+    windSpeed = json["WindSpeed"] ?? '無';
   }
 }
